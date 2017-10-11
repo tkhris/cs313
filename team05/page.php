6040e3c5b03971a6645a3c6e4146938660e3b369
Tyler Christensen <chr12061@byui.edu>
2017-10-10 18:01:22 -0600

    changed variables to connect to heroku db

--- team05/page.php
+++ team05/page.php
@@ -1,5 +1,5 @@
 <?php
-	$db_connection = pg_connect("host=localhost dbname=postgres user=postgres password=123");
+	$db_connection = pg_connect("host=ec2-54-225-88-191.compute-1.amazonaws.com dbname=d6g13tr9767jlv user=npasxllojhqdym password=df9180a75cdb4fb5f4da337bee7daa3ba79cdd39442e56d81c300bc6b895f241");
 
 	$result = pg_query($db_connection, "SELECT * FROM scriptures");
 
