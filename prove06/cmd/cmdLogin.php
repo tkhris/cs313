688d536345a810a42bab52c8dcc58e1f5ee08171
Tyler Christensen <chr12061@byui.edu>
2017-10-20 23:22:57 -0600

    fixed users not opening

--- prove06/cmd/cmdLogin.php
+++ prove06/cmd/cmdLogin.php
@@ -3,7 +3,7 @@ session_start();
 
 if (isset($_POST['username']) && isset($_POST['password']))
 {
-  require_once('../classes/User.php');
+  require_once('../classes/user.php');
   $user = new User;
   if(!$user->loadUserFromDb($_POST['username'], $_POST['password']))
   	header('Location: ../views/login.php');
